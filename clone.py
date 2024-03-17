c0042ed38caf3dc61743c5d9a175812ba2af0f2c
Manav Mahan Singh <manav.mahan.singh@live.in>
2024-03-17 23:23:26 +0100

    Update clone.py
    Added support for different repos

--- clone.py
+++ clone.py
@@ -2,13 +2,22 @@
 """Module to clone git repository using SSH on ephermal machines."""
 
 import os
+import sys
 from git import Repo
 
 ID_FILE = "id_rsa"
-REPO = "demo-gpt"
-UNAME = "manavmahan"
-KEY = os.environ['GIT_KEY']
 
+def get_input(arg_index):
+    if sys.argv[arg_index]:
+        return sys.argv[arg_index]
+    return input(f"Enter {key}")
+
+UNAME = get_input(1, "Enter GitHub Username:\t")
+REPO = get_input(2, "Enter Repository Name:\t")
+
+KEY = os.environ['GIT_KEY']
+if KEY is None:
+    KEY = input(f"Enter SSH Key to connect to GitHub\n")
 
 if __name__ == "__main__":
     with open(ID_FILE, 'w') as f:
